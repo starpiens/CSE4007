b50285f5a6ef5788dd95a403240e7cc60ab4eb0d
Jihun Kim <starlettkim@gmail.com>
2019-11-02 19:40:30 +0900

    Implement nQueensOpt.py

--- assignment4/nQueensOpt.py
+++ assignment4/nQueensOpt.py
@@ -6,6 +6,23 @@ N = int(input("N: "))
 
 start = time.time()
 # Variables
+X = [Int("col_%s" % col) for col in range(N)]
 
+# Constraints
+domain = [And(X[col] > 0, X[col] <= N) for col in range(N)]
+
+posConst = [
+    And(X[i] != X[j], X[j]-X[i] != j-i, X[j]-X[i] != i-j)
+    for i in range(N-1)
+    for j in range(i+1,N)
+]
+
+s = Solver()
+s.add(domain + posConst)
+
+if s.check() == sat:
+    m = s.model()
+    r = [m.evaluate(X[i]) for i in range(N)]
+    print(r)
 
 print("Elapsed time: ", time.time() - start, "s")
